1e39b857fe44e45a492b7e917f93c25b8bea3b53
Igaszczugiel <78268100+Igaszczugiel@users.noreply.github.com>
2021-03-13 12:27:56 +0000

    components under construction

---
+++ src/Components/Logo/logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './logo.css';
+
+function Logo() {
+  return (
+    <div class="top-header">
+      <div class="logo">
+      </div>
+      <div class="title">
+          <h1>Currency Converter</h1>
+          <h4>EUR / US DOLLAR / SWISS FRANC into PLN</h4>
+      </div>
+  </div>
+  );
+}
+export default Logo;
+
